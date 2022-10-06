08a8d02a9b2c82e7c3b9716d327dc12d8e54f22a
“sabrina-monster” <sabrina.rashid@monstar-lab.com>
2022-10-06 21:38:34 +0600

    loop assignment

---
+++ src/loop/OddNumber.java
@@ -0,0 +1,33 @@
+package loop;
+
+public class OddNumber {
+	
+	public static void main(String args[])   
+	{  
+		
+	int number=20;  int max = 0; 
+
+	System.out.print("List of odd numbers from 0 to "+number+": "); 
+	
+	for (int i=0; i<=number; i++)  
+		
+	{  
+ 
+	if (i%2!=0)   
+		
+	{ 	
+		if (i > max) {
+	        max = i;  
+	        
+		}
+			
+	System.out.print(i + " ");  	
+	
+	}
+	
+	}
+
+	System.out.print("maximum odd number " + max);
+
+	}
+}
